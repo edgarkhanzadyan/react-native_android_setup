adb41566f81a4bfea8d0b4059b81da286c8dfec2
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-02-24 18:01:48 +0000

    add setup shell for ubuntu in iterate

---
+++ setupLiveUbuntuForIterate/setup.sh
@@ -0,0 +1,12 @@
+echo "installing curl"
+sudo apt-get install curl
+echo "installing node"
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo "install git dependencies "
+sudo apt-get install git-man liberror-perl git
+echo "install atom"
+cd ~/Documents
+wget https://atom.io/download/deb
+sudo dpkg -i deb
+
